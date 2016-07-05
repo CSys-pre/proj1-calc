4d4f82c53e5d60e3132431a54f4c3c534b75ad52
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-07-05 16:25:58 +0200

    add `mod` functions - get the reminder of the division of 2 numbers

---
+++ mod.c
@@ -0,0 +1,8 @@
+#include "mod.h"
+
+int mod( int num1, int num2 )
+{
+	int result = num1 % num2;
+
+	return result;
+}
---
+++ mod.h
@@ -0,0 +1,9 @@
+#ifndef MOD_H_
+#define MOD_H_
+
+/* get the reminder of the divsion
+ */
+
+int mod( int num1, int num2 );
+
+#endif
