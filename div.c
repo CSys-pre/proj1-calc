e1136c33d846dece1d64b9b405e896a64335da00
basma11elmihy <basma.elmihy@gmail.com>
2016-07-05 18:19:51 +0200

    takes a number and returns its factorial

--- div.c
+++ div.c
@@ -1,1 +1,10 @@
+#include"div.h"
+int Div(int num1 , char sign ,int num2)
+{
+    int result;
+        if (sign == '/')
+          result = num1/num2;
 
+    return result;
+
+}
--- div.h
+++ div.h
@@ -1,1 +1,7 @@
+#ifndef DIV
+#define DIV
 
+//this function takes two numbers and returns its divsion.
+
+int Div(int num1 , char sign ,int num2);
+#endif // DIV
