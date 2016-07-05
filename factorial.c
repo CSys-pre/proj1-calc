4362b820636810797e5aef82eed8c8c3cec2fc6a
basma11elmihy <basma.elmihy@gmail.com>
2016-07-05 18:21:03 +0200

    get two numbers and returns its factorial

--- factorial.c
+++ factorial.c
@@ -1,1 +1,23 @@
+#include"factorial.h"
+long factorial(int fact , char sign)
+{
+    long result = 1;
 
+    if (sign == '!')
+      {
+        if (fact <= 1)
+            return 1;
+        else
+            {
+
+                int i;
+
+                for (i=1 ; i<=fact ; i++)
+                {
+                  result = result*i;
+                }
+            }
+    }
+    return result;
+
+}
--- factorial.h
+++ factorial.h
@@ -1,1 +1,7 @@
+#ifndef FACTORIAL
+#define FACTORIAL
 
+//this function takes a number and return its factorial
+long factorial(int fact , char sign);
+
+#endif // FACTORIAL
